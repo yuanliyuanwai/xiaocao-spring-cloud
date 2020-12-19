366e73b5f12cdd36cc46993306fbae670d6deb47
yuanliyuanwai <wanzhengchong@qq.com>
2020-12-19 20:54:35 +0800

    spring-cloud-consumer-hystrix-dashboard

--- hystrix-dashboard-turbine/spring-cloud-consumer-hystrix-dashboard/src/main/java/in/xiaocao/remote/HelloRemote.java
+++ hystrix-dashboard-turbine/spring-cloud-consumer-hystrix-dashboard/src/main/java/in/xiaocao/remote/HelloRemote.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name= "spring-cloud-producer", fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
 
-    @RequestMapping(value = "/hello")
-    public String hello(@RequestParam(value = "name") String name);
+    @RequestMapping(value = "/hello1")
+    String hello1(@RequestParam(value = "name") String name);
+    
+    @RequestMapping(value = "/hello2")
+    String hello2(@RequestParam(value = "name") String name);
 
 }
--- hystrix-dashboard-turbine/spring-cloud-consumer-hystrix-dashboard/src/main/java/in/xiaocao/remote/HelloRemoteHystrix.java
+++ hystrix-dashboard-turbine/spring-cloud-consumer-hystrix-dashboard/src/main/java/in/xiaocao/remote/HelloRemoteHystrix.java
@@ -1,15 +1,17 @@
 package in.xiaocao.remote;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import in.xiaocao.remote.HelloRemote;
 
 @Component
 public class HelloRemoteHystrix implements HelloRemote{
 
-    @Override
-    public String hello(@RequestParam(value = "name") String name) {
-        return "hello " +name+", this messge send failed ";
-    }
+	@Override
+	public String hello1(String name) {
+        return "hello1 " +name+", this messge send failed ";
+	}
+
+	@Override
+	public String hello2(String name) {
+		return "hello2 " +name+", this messge send failed ";
+	}
 }
